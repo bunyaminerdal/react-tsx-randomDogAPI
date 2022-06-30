e2a75afba9cf91017d4a0ead64e151549c42ff7c
leonidas131 <leonidas131@hotmail.com>
2022-06-30 11:11:44 +0300

    Update DogList.tsx

--- src/components/DogList.tsx
+++ src/components/DogList.tsx
@@ -5,13 +5,13 @@ import { useSelector } from "react-redux";
 import './DogList.css';
 
 const DogList = () => {
-      const dogList = useSelector((state: DogState) => state.reducer.dogs);
-
+  const dogList = useSelector((state: DogState) => state.reducer.dogs);
   return (
     <div className='doglistcontainer'>
-          {dogList.map((dog) => {
-          return <DogCard key={dog.message} message={dog.message} status={dog.status}/>
+      {dogList[0].message === "" ? <div> Welcoe to add Dog!</div> : <>{dogList.map((dog) => {
+        return <DogCard key={dog.message} message={dog.message} status={dog.status} />
       })}
+      </>}
     </div>
   )
 }
